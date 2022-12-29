90d2143beead8fa07fd44d33ae8c76d90290cca1
Arek <abudkowski@interia.pl>
2022-12-29 19:37:10 +0100

    delete folder 1

--- Knowledge/hashowanie.py
+++
@@ -1,6 +0,0 @@
-from hashlib import sha1
-
-text = 'admin123'
-
-my_hash = sha1(text.encode('UTF-8'))
-print(my_hash.hexdigest())
--- Knowledge/string_byte.py
+++
@@ -1,8 +0,0 @@
-text = 'szynszylka'
-
-text_in_bytes = text.encode()
-print (type(text_in_bytes))
-print(text_in_bytes)
-text_in_str = text_in_bytes.decode()
-print(type(text_in_str))
-print(text_in_str)

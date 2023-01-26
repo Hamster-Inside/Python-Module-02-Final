7eac59755121f1608747fa90fd26015ee300ef8f
Arek <abudkowski@interia.pl>
2023-01-26 06:37:24 +0100

    finished

--- files_manager.py
+++ files_manager.py
@@ -1,26 +1,47 @@
+""" File manager """
 import typing
 
 
 class FilesManager:
-
+    """ Class for operating on files """
     def __init__(self, directory="Secrets/"):
-        self.DIRECTORY_OF_FILES = directory
-        self.PASSWORDS_FILE_NAME = "passwords.txt"
-        self.SAFE_PASSWORDS_FILE_NAME = "safe_passwords.txt"
-        self.UNSAFE_PASSWORDS_FILE_NAME = "unsafe_passwords.txt"
-        self.create_safe_and_unsafe_files()
+        self.dir = directory
+        self.passwords_file_name = "passwords.txt"
+        self.safe_passwords_file_name = "safe_passwords.txt"
+        self.unsafe_passwords_file_name = "unsafe_passwords.txt"
+        self._create_safe_and_unsafe_files()
         self.passwords_list = []
 
-    def create_safe_and_unsafe_files(self) -> None:
+    def _create_safe_and_unsafe_files(self) -> None:
+        """ Creates files if they do not exist"""
         with open(
-                self.DIRECTORY_OF_FILES + self.PASSWORDS_FILE_NAME, encoding="UTF-8") as input_file, open(
-            self.DIRECTORY_OF_FILES + self.SAFE_PASSWORDS_FILE_NAME, mode='w', encoding="UTF-8") as safe_output, open(
-            self.DIRECTORY_OF_FILES + self.UNSAFE_PASSWORDS_FILE_NAME, mode='w', encoding="UTF-8") as unsafe_output:
-            pass
+                self.dir + self.passwords_file_name,
+                mode='a+', encoding="UTF-8") as input_file, open(
+                self.dir + self.safe_passwords_file_name,
+                mode='w', encoding="UTF-8"), open(
+                self.dir + self.unsafe_passwords_file_name,
+                mode='w', encoding="UTF-8"):
+            input_file.seek(0)
+            if input_file.readline() == "":
+                input_file.write("TestPassword#000")
 
     def get_list_of_passwords(self) -> typing.List:
-        """Returns list of passwords from txt file"""
+        """ Returns list of passwords from txt file """
+        if len(self.passwords_list) == 0:
+            with open(self.dir + self.passwords_file_name,
+                      mode='r', encoding="UTF-8") as input_file:
+                for line in input_file.readlines():
+                    self.passwords_list.append(line.strip('\n'))
         return self.passwords_list
 
+    def add_to_safe_passwords_file(self, text):
+        """ adds password to safe passwords file it is a safe password """
+        with open(self.dir + self.safe_passwords_file_name,
+                  mode='a', encoding='UTF-8') as input_file:
+            input_file.write(text + '\n')
 
-
+    def add_to_unsafe_passwords_file(self, text):
+        """ adds password to unsafe passwords file it is a unsafe password """
+        with open(self.dir + self.unsafe_passwords_file_name,
+                  mode='a', encoding='UTF-8') as input_file:
+            input_file.write(text + '\n')
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """ Small Script to show if password has leaked """
-from password_validators.password_validator import PasswordValidator
-import logging
+from password_validators.password_validator import PasswordValidator, ValidationError
+from files_manager import FilesManager
 
-logging.basicConfig(filename=r'Logs\module_02_logs.log', encoding='utf-8', level=logging.INFO)
+current_file = FilesManager()
 
-validator = PasswordValidator('ZAQ!2wsx')
-
-print(validator.is_valid())
-
-
+for password in current_file.get_list_of_passwords():
+    validator = PasswordValidator(password)
+    try:
+        validator.is_valid()
+        current_file.add_to_safe_passwords_file(password)
+    except ValidationError as error:
+        current_file.add_to_unsafe_passwords_file(password)
+        print(f'{password} -> {error}')
